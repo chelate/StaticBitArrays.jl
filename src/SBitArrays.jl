83c845685b8366cc77e96cb4b75e56b53c8b6ad7
Rafael Schouten <rafaelschouten@gmail.com>
2021-06-20 23:27:59 +1000

    use tuple_prod from StaticArrays

--- src/SBitArrays.jl
+++ src/SBitArrays.jl
@@ -1,31 +1,30 @@
 module SBitArrays
-using Core: ReturnNode
-export SBitVector, SBitMatrix, SBitArray
 
 using StaticArrays
+const SA = StaticArrays
 
-struct SBitArray{S,M,T<:Unsigned,N} <: StaticArray{S,Bool,N}
+export SBitVector, SBitMatrix, SBitArray
+
+struct SBitArray{S,M,T<:Unsigned,N,L} <: StaticArray{S,Bool,N}
     # type signiture: {Length, ChunksLength, ChunksType}
     chunks::SVector{M,T}
     function SBitArray{S}(s::SVector{M,T}) where {S,M,T}
          S <: Tuple || error("type parameter S: $S must be a Tuple type of Int, e.g. `SBitArray{Tuple{4,5}}`")
-         size = fieldtypes(S)
-         N = length(size)
-         len = prod(size)
-         if length(s) != nchunks(T, len)
+         N = SA.tuple_length(S)
+         L = SA.tuple_prod(S)
+         if length(s) != nchunks(T, L)
              error("register/length mismatch")
          end
-         new{S,M,T,N}(s)
+         new{S,M,T,N,L}(s)
     end
 end
 
-const SBitVector{L} = SBitArray{Tuple{L}} where L
-const SBitMatrix{A,B} = SBitArray{Tuple{A,B}} where {A,B}
+const SBitVector{S1} = SBitArray{Tuple{S1}} where S1
+const SBitMatrix{S1,S2} = SBitArray{Tuple{S1,S2}} where {S1,S2}
 
 # Convenience constructors for an array of booleans, not type stable
 function SBitArray{S}(::Type{T}, a::AbstractArray{Bool}) where {S,T<:Unsigned}
-    size = fieldtypes(S)
-    L = prod(size)
+    L = SA.tuple_prod(S)
     M = nchunks(T, L)
     chunks = zeros(T,M)
     ii = 0
@@ -46,6 +45,8 @@ Base.IndexStyle(::Type{<:SBitArray}) = IndexLinear()
 Base.getindex(s::SBitArray{S,M,T}, ind::Int) where {S,M,T} = @inbounds readbit_mod(s.chunks[cld(ind, sizeof(T)*8)], ind)
 # moded version of readbit
 readbit_mod(x::T, loc::Int) where {T<:Unsigned} = (x >> (mod1(loc, sizeof(T)*8) - 1)) & one(T) == one(T)
+
+nchunks(T, l) = cld(l, sizeof(T) * 8)
 
 function Base.iterate(s::SBitArray{S,M,T}, state=(1, s.chunks[1])) where {S,M,T} 
     (ind, val::T) = state
